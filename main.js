62724527b14452fe0c914348d6b970e6724c3b6b
lifaa12 <andy0312huang@gmail.com>
2024-09-24 02:04:24 +0800

    index

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+import './node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './assets/scss/all.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
-console.log("Hello world!");
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1.3,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 2000,  
+    disableOnInteraction: false,  
+    pauseOnMouseEnter: true,   
+  },
+  breakpoints:{
+    992:{
+      slidesPerView: 2.2,
+      centeredSlides: true,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
