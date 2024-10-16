6338d633984b2bdbf2a43eb73bbe0181d1bfcbd2
lifaa12 <andy0312huang@gmail.com>
2024-10-16 13:34:51 +0800

    html-tag,aos,survey-delete

--- assets/js/survey-create.js
+++ assets/js/survey-create.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const surveyList = document.querySelector(".survey-create-area");
   const surveyCreateCollapse = document.getElementById('surveyCreateCollapse');
 
-
   // 初始化 Collapse
   const collapse = new bootstrap.Collapse(surveyCreateCollapse, {
     toggle: false
@@ -20,20 +19,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   ];
   let showData = [];
-  addSurvey();
 
   // 新增問卷
   function addSurvey() {
     const surveyAdd = document.querySelectorAll(".survey-add li a");
-    surveyAdd.forEach((item, index) => {
+    surveyAdd.forEach((item) => {
       item.addEventListener("click", (e) => {
         e.preventDefault();
-        if (e.target.text !== "單行文字") {
+        if (e.target.textContent !== "單行文字") {
           return;
         }
         let obj = {};
-        obj.type = data[index].type;
-        obj.input = data[index].input;
+        obj.type = data[0].type;
+        obj.input = data[0].input;
+        obj.id = Date.now();
+        obj.titleValue = "";
         showData.push(obj);
         renderData();
         collapse.hide();
@@ -41,21 +41,45 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // 刪除
+  surveyList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("bi-x-circle")) {
+      let num = e.target.dataset.id;
+      let idx = showData.findIndex((item) => item.id == num);
+      showData.splice(idx, 1);
+      renderData();
+    };
+  });
+
+  // 保留已輸入內容
+  function saveValues() {
+    let inputCard = document.querySelectorAll(".survey-create-area .card");
+    inputCard.forEach((item) => {
+      let id = item.closest('li').getAttribute('data-id');
+      let itemIdx = showData.findIndex(item => item.id == id);
+      if (itemIdx !== -1) {
+        let title = item.querySelector(".card-body input.border-primary-400");
+        showData[itemIdx].titleValue = title.value;
+      };
+    });
+  }
+
   // 渲染
   function renderData() {
+    saveValues();
     let str = "";
     showData.forEach((item) => {
       str += `
-        <li>
+        <li data-id="${item.id}">
           <div class="card">
             <div class="card-header">
               <p>${item.type}</p>
               <a href="#" class="surveycard-delete">
-                <i class="bi bi-x-circle fs-5"></i>
+                <i class="bi bi-x-circle fs-5" data-id="${item.id}"></i>
               </a>
             </div>
             <div class="card-body">
-              <input class="form-control border-primary-400 mb-4" type="text" placeholder="標題">
+              <input class="form-control border-primary-400 mb-4" type="text" placeholder="標題" value="${item.titleValue || ""}">
               ${item.input}
             </div>
           </div>
@@ -63,4 +87,5 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     surveyList.innerHTML = str;
   }
+  addSurvey();
 });
